ab6386a8a2eb00f8e38bf14b2483a9c50b1eed37
jvazquez30 <josevazquez881@gmail.com>
2024-05-13 21:41:24 -0700

    Added new code

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export function useDarkMode() {
+    const [dark, setDark] = useLocalStorage('darkmode', false)
+    return [dark, setDark]
+
+}
